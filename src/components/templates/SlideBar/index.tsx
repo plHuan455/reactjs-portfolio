8d2ee36a24e29946e58a01c6237fbfba8c9c06f8
Huan Pham Long <huan.pl@novaeforce.vn>
2022-09-22 14:15:26 +0700

    a

--- src/components/templates/SlideBar/index.tsx
+++ src/components/templates/SlideBar/index.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 
 export interface SlideBarProps {
+  logoSrc?: string;
 }
 
-export default function SlideBar (props: SlideBarProps) {
+export default function SlideBar ({logoSrc}: SlideBarProps) {
   return (
     <div className="t-slideBar">
+        {logoSrc && <div className="t-slideBar_logo">
+          <img src={logoSrc} alt="empty" />
+        </div>}
         
     </div>
   );
--- src/pages/TestPage/index.tsx
+++ src/pages/TestPage/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import SlideBar from '../../components/templates/SlideBar';
 
 export interface TestPageProps {
 }
@@ -6,7 +7,7 @@ export interface TestPageProps {
 export default function TestPage (props: TestPageProps) {
   return (
     <div>
-      
+      <SlideBar />
     </div>
   );
 }
